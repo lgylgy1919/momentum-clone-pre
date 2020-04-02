818af840f2244b452bb53b41505cdefdaca45649
lgylgy1919 <59249964+lgylgy1919@users.noreply.github.com>
2020-04-03 02:06:58 +0900

    last chapter

--- clock.js
+++ clock.js
@@ -6,7 +6,7 @@ function getTime() {
   const hours = date.getHours();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours < 10 ? `$0{hours}` : hours}:${
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
     minutes < 10 ? `0${minutes}` : minutes
   }:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
